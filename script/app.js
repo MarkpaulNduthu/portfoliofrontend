ed43f96b31c52105200da44ddfbfd5246b22f4a0
MarkpaulNduthu <paulmarkmwema@gmail.com>
2025-03-29 10:38:10 +0300

    changed directory

--- script/app.js
+++ script/app.js
@@ -63,7 +63,7 @@ function validateandsubmit() {
     // formdata.append("email", email);
     // formdata.append("message", message);
     try {
-        fetch("http://mwemaportfolio.great-site.net/php/index.php",
+        fetch("https://mwemaportfolio.great-site.net/htdocs/index.php",
             {
                 method: 'POST',
                 body: formdata,
